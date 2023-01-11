c7fe190c868126dd1a0d92088c76456e5ae5f1e1
Sivasathwik <122215023+Sivasathwik@users.noreply.github.com>
2023-01-11 09:08:00 -0800

    Add files via upload

---
+++ EASY 5 QUES.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+	float inpu,s,c;
+	printf("Enter a number you want to square and cube : ");
+	scanf("%f",&inpu);
+	printf("Square of given number is : %f\n",inpu*inpu);
+	printf("Cube of given number is %f",inpu*inpu*inpu);
+}
